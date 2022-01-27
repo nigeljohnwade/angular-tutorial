d37d2be56358147303ef69920a5445075046ebee
Nigel Wade <nigel.wade@beeksgroup.com>
2022-01-27 17:15:55 +0000

    Add long description to items

--- src/app/items.ts
+++ src/app/items.ts
@@ -2,22 +2,26 @@ export interface Item {
   id: number;
   name: string;
   description: string;
+  longDescription: string;
 }
 
 export const items = [
   {
     id: 1,
     name: 'Item 1',
-    description: 'description 1'
+    description: 'description 1',
+    longDescription: 'Long description 1',
   },
   {
     id: 2,
     name: 'Item 2',
-    description: 'description 2'
+    description: 'description 2',
+    longDescription: 'Long description 2',
   },
   {
     id: 3,
     name: 'Item 3',
-    description: 'description 3'
+    description: 'description 3',
+    longDescription: 'Long description 3',
   }
 ];
