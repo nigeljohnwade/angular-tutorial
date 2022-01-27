051944ba0b3114656788f5540efdfa0e3ad0917a
Nigel Wade <nigel.wade@beeksgroup.com>
2022-01-27 16:24:28 +0000

    Initial commit

---
+++ src/app/item-list/item-list.component.ts
@@ -0,0 +1,20 @@
+import {Component, OnInit} from '@angular/core';
+
+import {items} from '../items';
+
+@Component({
+  selector: 'app-item-list',
+  templateUrl: './item-list.component.html',
+  styleUrls: ['./item-list.component.css']
+})
+export class ItemListComponent implements OnInit {
+
+  constructor() {
+  }
+
+  items = items
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/items.ts
@@ -0,0 +1,23 @@
+export interface Item {
+  id: number;
+  name: string;
+  description: string;
+}
+
+export const items = [
+  {
+    id: 1,
+    name: 'Item 1',
+    description: 'description 1'
+  },
+  {
+    id: 2,
+    name: 'Item 2',
+    description: 'description 2'
+  },
+  {
+    id: 3,
+    name: 'Item 3',
+    description: 'description 3'
+  }
+];
